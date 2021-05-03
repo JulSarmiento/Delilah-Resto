68a8e161fd3bccd6e420e07fca0ae7c41e6909ed
Julieth Sarmiento <jasa1999@hotmail.com>
2021-05-03 10:05:09 -0500

    Creating repository. Almost finished project. Order part in progess

---
+++ src/middlewares/order-body-validator.js
@@ -0,0 +1,11 @@
+/**
+ * Product body validator middleware
+ *
+ * @param {e.Request} request - The Request
+ * @param {e.Response} response - The Response
+ * @param {Function} next - Trigger next route
+ */
+ module.exports = (request, response, next) => {
+    next()
+}
+  
